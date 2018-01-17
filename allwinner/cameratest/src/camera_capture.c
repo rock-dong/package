e69417fd05bda897378e6bcfe8e158c0be41e202
rockdong <xinyukey@163.com>
2018-01-17 14:29:53 +0800

    add jpg/rgb convertion, make mincomtest more flexibe

--- allwinner/cameratest/src/camera_capture.c
+++ allwinner/cameratest/src/camera_capture.c
@@ -15,6 +15,11 @@
  */
 
 #include "camera_capture.h"
+#include <jpeglib.h>
+int minmax(int min, int v, int max)
+{
+  return (v < min) ? min : (max < v) ? max : v;
+}
 
 int init_capture(void *arg)
 {
@@ -146,16 +151,74 @@ int capture_quit(void *arg)
 	return 0;
 }
 
+uint8_t* yuyv2rgb(uint8_t* yuyv, uint32_t width, uint32_t height)
+{
+  FILE *fp;
+  int iim;
+  uint8_t* rgb = (uint8_t*)calloc(width * height * 3, sizeof (uint8_t));
+  printf("yuyv2rgb\n");
+  for (size_t i = 0; i < height; i++) {
+    for (size_t j = 0; j < width; j += 2) {
+      size_t index = i * width + j;
+      int y0 = yuyv[index * 2 + 0] << 8;
+      int u = 0x00;
+      int y1 = yuyv[index * 2 + 2] << 8;
+      int v = 0x00;
+      rgb[index * 3 + 0] = minmax(0, (y0 + 359 * v) >> 8, 255);
+      rgb[index * 3 + 1] = minmax(0, (y0 + 88 * v - 183 * u) >> 8, 255);
+      rgb[index * 3 + 2] = minmax(0, (y0 + 454 * u) >> 8, 255);
+      rgb[index * 3 + 3] = minmax(0, (y1 + 359 * v) >> 8, 255);
+      rgb[index * 3 + 4] = minmax(0, (y1 + 88 * v - 183 * u) >> 8, 255);
+      rgb[index * 3 + 5] = minmax(0, (y1 + 454 * u) >> 8, 255);
+    }
+  }
+  return rgb;
+}
+void jpeg(FILE* dest, uint8_t* rgb, uint32_t width, uint32_t height, int quality)
+{
+  JSAMPARRAY image;
+  image = (JSAMPARRAY)calloc(height, sizeof (JSAMPROW));
+  for (size_t i = 0; i < height; i++) {
+    image[i] = (uint8_t*)calloc(width * 3, sizeof (JSAMPLE));
+    for (size_t j = 0; j < width; j++) {
+      image[i][j * 3 + 0] = rgb[(i * width + j) * 3 + 0];
+      image[i][j * 3 + 1] = rgb[(i * width + j) * 3 + 1];
+      image[i][j * 3 + 2] = rgb[(i * width + j) * 3 + 2];
+    }
+  }
+  struct jpeg_compress_struct compress;
+  struct jpeg_error_mgr error;
+  compress.err = jpeg_std_error(&error);
+  jpeg_create_compress(&compress);
+  jpeg_stdio_dest(&compress, dest);
+  compress.image_width = width;
+  compress.image_height = height;
+  compress.input_components = 3;
+  compress.in_color_space = JCS_RGB;
+  jpeg_set_defaults(&compress);
+  jpeg_set_quality(&compress, quality, TRUE);
+  jpeg_start_compress(&compress, TRUE);
+  jpeg_write_scanlines(&compress, image, height);
+  jpeg_finish_compress(&compress);
+  jpeg_destroy_compress(&compress);
+  for (size_t i = 0; i < height; i++) {
+    free(image[i]);
+  }
+  free(image);
+}
 void *Capture_Test_Thread(void *arg)
 {
 	enum v4l2_buf_type type;
 	struct timeval tv;
+	struct timeval timestart;
 	fd_set fds;
 	struct v4l2_buffer buf;
 	int ret;
 	int np = 0;
 	long long timestamp_now, timestamp_save;
 	char source_data_path[30];
+	char out_name[64];
+        unsigned char* rgb;
 
 	hawkview_handle *hv = (hawkview_handle*)arg;
 
@@ -172,6 +235,8 @@ void *Capture_Test_Thread(void *arg)
 	memset(&buf, 0, sizeof(struct v4l2_buffer));
 	buf.type   = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 	buf.memory = V4L2_MEMORY_MMAP;
+        gettimeofday(&timestart, NULL);
+        printf("capture start no save ==> %ld:%ld \n",timestart.tv_sec, timestart.tv_usec);
 
 	while (np < hv->detect.frame_num) {
 		hv_msg("capture num is [%d]\n",np);
@@ -198,16 +263,27 @@ void *Capture_Test_Thread(void *arg)
 		hv_msg("the interval of two frames is %lld ms\n", timestamp_now - timestamp_save);
 		timestamp_save = timestamp_now;
 
-		sprintf(source_data_path, "/tmp/%s%d%s", "source_data", np+1, ".yuv");
+		if (hv->capture.cap_fmt == V4L2_PIX_FMT_NV21) {
+		    sprintf(source_data_path, "%s/%s%d%s", hv->detect.path, "source_data", np+1, ".yuv");
         save_frame_to_file(source_data_path, hv->capture.buffers[buf.index].start, buf.bytesused);
 
+		}else if( hv->capture.cap_fmt == V4L2_PIX_FMT_YUYV) {
+		    rgb = yuyv2rgb(hv->capture.buffers[buf.index].start, hv->capture.cap_w, hv->capture.cap_h);
+		}
 		/* qbuf */
 		if (ioctl(hv->detect.videofd, VIDIOC_QBUF, &buf) == 0)
 			hv_msg("************QBUF[%d] FINISH**************\n",buf.index);
 
+		if( hv->capture.cap_fmt == V4L2_PIX_FMT_YUYV) {
+		    sprintf(out_name,"%s/%011lld.jpg",hv->detect.path,timestamp_now);
+		    FILE* out = fopen(out_name, "w");
+		    jpeg(out, rgb, hv->capture.cap_w, hv->capture.cap_h, 100);
+		    fclose(out);
+		}
 		np++;
 	}
-	hv_msg("capture thread finish !\n");
+	gettimeofday(&timestart, NULL);
+	hv_msg("capture thread finish %ld:%ld !\n", timestart.tv_sec, timestart.tv_usec);
 	//capture_quit(hv);
 	return (void*)0;
 }
--- allwinner/mincomtest/src/main.c
+++ allwinner/mincomtest/src/main.c
@@ -8,10 +8,14 @@
 #include <termios.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <sys/time.h>
 
+#define TEST_DATA_LEN 98
 
 int fd;
 int nread,i,nsend;
+long totalReadNum = 0;
+long errIndex = 0;
 char writedata[]="welcome to R16!\n";
 char uart1testdata[] = "uart port 1 test data";
 char uart2testdata[] = "uart port 2 test data";
@@ -21,6 +25,28 @@ char buffer[1024];
 const int READ_THREAD_ID = 0;
 const int SEND_THREAD_ID = 1;
 pthread_t thread[2];
+
+// 0 is output, 1 is input
+int direction = 0;
+int bandwidth = 0;
+int portnum = 0;
+int looptestResult = 0;
+
+
+const char send_buf[TEST_DATA_LEN]={0xaa,0x55,0x5d,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x0f,0x0e,0x0d,0x0c,0x0b,\
+		    0x0a,0x09,0x08,0x07,0x06,0x05,0x04,0x03,0x02,0x01,0x00,0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,\
+		    0x0c,0x0d,0x0e,0x0f,0x0f,0x0e,0x0d,0x0c,0x0b,0x0a,0x09,0x08,0x07,0x06,0x05,0x04,0x03,0x02,0x01,0x00,0x00,0x01,0x02,\
+		    0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0a,0x0b,0x0c,0x0d,0x0e,0x0f,0x0f,0x0e,0x0d,0x0c,0x0b,0x0a,0x09,0x08,0x07,0x06,\
+		    0x05,0x04,0x03,0x02,0x01,0xdf};
+
+
+char comp_buffer[TEST_DATA_LEN]= {0x00};
+int headerFlag = 0;
+int validLen = 0;
+int testloop = 0;
+
+
+int findFirstPackage(int handle);
 
 int set_opt(int fd,int nSpeed, int nBits, char nEvent, int nStop)
 {
@@ -76,9 +102,25 @@ int set_opt(int fd,int nSpeed, int nBits, char nEvent, int nStop)
 			cfsetispeed(&newtio, B9600);
 			cfsetospeed(&newtio, B9600);
 			break;
+		case 19200:
+			cfsetispeed(&newtio, B19200);
+			cfsetospeed(&newtio, B19200);
+			break;
+		case 38400:
+			cfsetispeed(&newtio, B38400);
+			cfsetospeed(&newtio, B38400);
+			break;
+		case 57600:
+			cfsetispeed(&newtio, B57600);
+			cfsetospeed(&newtio, B57600);
+			break;
 		case 115200:
 			cfsetispeed(&newtio, B115200);
 			cfsetospeed(&newtio, B115200);
+			break;
+		case 1500000:
+			cfsetispeed(&newtio, B1500000);
+			cfsetospeed(&newtio, B1500000);
 			break;
 		default:
 			cfsetispeed(&newtio, B9600);
@@ -188,7 +230,7 @@ void* com_read(void * p)
 }
 
 
-void* uart1_test(void * p)
+void* uart_test(void * p)
 {
     char* expectdata = (char *)p;
     //printf("uart1_test...");
@@ -205,27 +247,6 @@ void* uart1_test(void * p)
 	}
     }else{
         printf("no data, uart 1 test fail\n"); 
-    }
-    
-}
-
-void* uart2_test(void * p)
-{
-    char* expectdata = (char *)p;
-    //printf("uart2_test...");
-    sleep(1);
-    nread = read(fd,buffer,strlen(expectdata)+1);
-    //printf("read return %d bytes\n", nread);
-
-    if(nread>0){
-        //printf("recvcontent: %s\n",buffer);
-	if(0 == strncmp(buffer, expectdata, strlen(expectdata))){
-            printf("uart 2 test success\n");
-	}else{
-	    printf("data corrupt, uart 2 test fail\n");
-	}
-    }else{
-        printf("no data, uart 2 test fail\n"); 
     }
     
 }
@@ -264,72 +285,302 @@ void* com_send_once(void * p)
         } 
 }	
 
-int main(void)
+
+void* looptest_send(void * p)
 {
-    printf("test start...\n");
-    if((fd=open_port(fd,2))<0){
+    //char * uartdata = (char*)p;
+    char* uartdata = (char *)malloc(TEST_DATA_LEN+1);
+    int len = 0;
+    int loop = testloop;
+    char tmp = 0;
+    struct timeval time1;
+    memcpy(uartdata, p, TEST_DATA_LEN);
+    while(loop > 0){
+   
+	len = 0;
+/*
+        if(loop%6 == 0){
+	    tmp = uartdata[2];
+	    uartdata[2] = loop%128;
+	}
+*/
+	while(len < TEST_DATA_LEN) {    
+	    nsend=write(fd,uartdata+len,TEST_DATA_LEN-len);
+            gettimeofday(&time1, NULL);
+            printf("loop  nsend:%d, %ld:%ld \n",nsend, time1.tv_sec, time1.tv_usec);
+	
+            if(nsend<0) {
+	        printf("send  error\n");
+            } else {
+                //printf("Send  OK\n"); 
+            }  
+
+	    len = nsend + len;
+	}
+/*	
+	if(loop % 6 == 0){
+	    uartdata[2] = tmp;
+	}
+*/
+	usleep(20000);
+	loop--;
+    } 
+
+}	
+
+
+
+void* looptest_receive(void * p)
+{
+    char* expectdata = (char *)p;
+    //sleep(1);
+    int len = 0;
+    int ret = 0;
+    int loop =  testloop;
+    struct timeval time1;
+    
+    printf("looptest_receive ... \n");
+    
+    while(loop > 0) {
+        
+	len = 0;
+
+        ret = findFirstPackage(fd);
+        if(ret == 0){
+	    looptestResult++;
+	    loop--;
+	} else if(ret == -1){
+	    loop--;	
+	} else {
+	    	
+	}
+
+	while(len < TEST_DATA_LEN) {
+	    nread = read(fd, buffer + len, TEST_DATA_LEN-len);
+        //    gettimeofday(&time1, NULL);
+            //printf("loop read return %d bytes\n", nread);
+
+            if(nread>0){
+               //printf("recv %d bytes, content: %x, %x %x, %ld:%ld\n",nread, buffer[len], buffer[len+1], buffer[len+2], time1.tv_sec, time1.tv_usec);
+	        totalReadNum += nread;
+            }else{
+               //printf("no data\n"); 
+            }
+
+	    len = nread + len;
+
+	}
+
+        ret = checkValue(send_buf, buffer, TEST_DATA_LEN);        
+        if(ret == 0){
+	    looptestResult++;
+	}
+
+        gettimeofday(&time1, NULL);
+	
+	if(ret == 0) {
+	    printf("pass %d %ld:%ld\n", loop, time1.tv_sec, time1.tv_usec);
+	}else{
+	    printf("fail %d %ld:%ld\n", loop, time1.tv_sec, time1.tv_usec);
+	    headerFlag = 0;
+	    ret = findFirstPackage(fd);	
+	    if(ret == 0){
+	        looptestResult++;
+		loop--;
+	    } else if(ret == -1){
+	        loop--;
+	    } else {
+	    
+	    }
+	}
+
+	loop--;
+    }
+    
+}
+
+
+/***********************************/
+/*         return value            */
+/*  0 is read 98 and value is ok   */
+/*  -1 is read 98 and value is fail*/
+/*   1 is skip read                */
+/***********************************/
+
+int findFirstPackage(int handle)
+{
+    int firstNum = 0, len = 0, ret = 0;
+
+    //headerFlag = 0;
+    if(headerFlag == 0){
+        printf("sync %ld +++ \n", totalReadNum);
+    }else{
+        ret = 1;
+    }
+
+    while(headerFlag == 0) {
+        nread = read(handle, buffer, TEST_DATA_LEN);
+
+	if(nread > 0){
+	    firstNum = findHeader(buffer, nread);    
+	    if(firstNum < nread){
+                headerFlag = 1;
+		memcpy(comp_buffer, buffer+firstNum, nread-firstNum);
+		printf("find 0xaa at %ld \n", firstNum + totalReadNum);
+            }else{
+	  
+            }
+	    totalReadNum += nread;
+        }else{
+	    
+	}
+
+	if(headerFlag == 1) {
+	    len = nread - firstNum;
+            while(len < TEST_DATA_LEN){
+                nread = read(handle, comp_buffer+len, TEST_DATA_LEN-len);
+                if(nread > 0){
+		   totalReadNum += nread; 
+		}else{
+		    
+		}
+
+                len = nread + len;
+            }
+
+            ret = checkValue(send_buf, comp_buffer, TEST_DATA_LEN);
+            printf("first complete package arrive , result %d\n", ret);
+		
+            printf("sync %ld --- \n", totalReadNum);
+	    
+        }
+	
+    }
+
+    return ret;
+}
+
+
+int findHeader(char* buf, int len)
+{
+     int i = 0;
+     while(i < len){
+        if(buf[i] == 0xaa){
+            break;
+        }
+	i++;
+     }
+     
+     return i;
+
+}
+
+
+int checkValue(char* src, char* dest, int length)
+{
+     int i = 0;
+     int ret = 0;
+     while(i < length) {
+         if(src[i] != dest[i]) {
+	    printf("index %d wrong , %x -> %x \n", i, src[i], dest[i]);
+	    printf("%x ,%x, %x, %x \n", dest[0],dest[1],dest[2],dest[3]);
+            ret = -1;
+            break;	    
+	 }	 
+         i++;
+     }
+
+     return ret;
+      
+}
+
+
+
+
+int parse(char* in, int inputLen, char* tail, int* tailLen)
+{
+    int i = 0;
+    do {
+        if((in[i] == 0xaa) && (in[i+1] == 0x55) && (in[i+2] == 0x5d)) {
+	    
+	}
+    }while(i < inputLen);	    
+
+}
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+    struct timeval timestart;
+    int i = 0;
+    gettimeofday(&timestart, NULL);
+    printf("receive test start %ld:%ld \n", timestart.tv_sec, timestart.tv_usec);
+
+    if(argc != 5){
+        printf("wrong parameter\n");
+        printf("argv[1]: 0 => output, 1 => input\n");
+        printf("argv[2]: bandwidth\n");
+        return -1;
+    }
+
+    direction = atoi(argv[1]);
+    bandwidth = atoi(argv[2]);
+    portnum = atoi(argv[3]);
+    testloop = atoi(argv[4]);
+
+    printf("user input direction %d, %d, port %d\n", direction, bandwidth, portnum, testloop);
+
+    if((fd=open_port(fd,portnum))<0){
         perror("open_port error");
 	
         return 1;
     }
 		
-    if((i=set_opt(fd,115200,8,'N',1))<0){
+    if((i=set_opt(fd,bandwidth,8,'N',1))<0){
         perror("set_opt error");
         return 1;
     }
 		
+    
     //printf("fd=%d\n",fd);
-		
-    if(pthread_create( &thread[READ_THREAD_ID], NULL, uart1_test, (void *)uart1testdata)== 0){
-        //printf(" create read thread success!\n");
-    } else {
-        printf(" create read thread fail!");
+
+    if(direction == 1 || direction == 2){	
+        if(pthread_create( &thread[READ_THREAD_ID], NULL, looptest_receive, NULL)== 0){
+            printf(" create read thread success!\n");
+        } else {
+            printf(" create read thread fail!");
+        }
+    }
+
+    if(direction == 0 || direction == 2){
+        if(pthread_create( &thread[SEND_THREAD_ID], NULL, looptest_send, (void *)send_buf)==0){
+            printf(" create send thread success!\n");
+        } else {
+            printf(" create send thread fail!");
+        }
     }
 
 
-    if(pthread_create( &thread[SEND_THREAD_ID], NULL, com_send_once, (void *)uart1testdata)==0){
-        //printf(" create send thread success!\n");
-    } else {
-        printf(" create send thread fail!");
+    if(testloop < 100){
+       sleep(3);
+    }else {
+       int duration = (testloop/100+1)*2;
+       sleep(duration);    
     }
-		
-    sleep(2);
-	
-    close(fd);
-
-
-    if((fd=open_port(fd,3))<0){
-        perror("open_port error");	
-        return 1;
-    }
-		
-    if((i=set_opt(fd,115200,8,'N',1))<0){
-        perror("set_opt error");
-        return 1;
-    }
-		
-    //printf("fd=%d\n",fd);
-		
-    if(pthread_create( &thread[READ_THREAD_ID], NULL, uart2_test,(void *)uart2testdata)== 0){
-        //printf(" create read thread success!\n");
-    } else {
-        printf(" create read thread fail!");
-    }
-
-
-    if(pthread_create( &thread[SEND_THREAD_ID], NULL, com_send_once,(void *)uart2testdata)==0){
-        //printf(" create send thread success!\n");
-    } else {
-        printf(" create send thread fail!");
-    }
-		
-
-    sleep(2);
 
     close(fd);
-
-
-    printf("test end");
+    gettimeofday(&timestart, NULL);
+    
+    if(looptestResult == testloop){
+        printf("test success\n");
+    }else{
+        printf("test fail\n");
+    }
+    
+    printf("test end %ld:%ld \n",timestart.tv_sec, timestart.tv_usec);
 
     return 1;
 }
